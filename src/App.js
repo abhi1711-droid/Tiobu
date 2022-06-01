c8ca79f8ea23bc282c70e67178c6e692a4523954
abhi1711-droid <abhisheksinha1709@gmail.com>
2022-06-01 10:03:56 +0530

    reload fun

--- src/App.js
+++ src/App.js
@@ -5,29 +5,17 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import AllCreators from './AllCreators'
 import Donations from './Donations'
 import TModal from './TModal'
-import BasicModal from './modal'
 
 function App() {
-
-
- 
-
   return (
     <>
-
-    
       <BrowserRouter>
         <Switch>
           <Route exact path= "/" component ={AllCreators} /> 
           <Route exact path = "/donations" component = {Donations} />
           <Route exact path = "/submit" component={TModal} />
-          <Route exact path = "/modal" component = {BasicModal} />
         </Switch>
-      </BrowserRouter>
-
-      {/* data */}
-
-     
+      </BrowserRouter>     
     </>
   )
 }
